9e89a691111257836adfd10fc27f428da9233f4c
eugene <eugeneholenstein@gmail.com>
2022-12-08 22:54:35 +1100

    commit tree fede12f50fa40153b43b8d9b3333e73a42a3d2fe

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+// expect(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")).to.deep.eq([
+//     "Thank you, Guadalupe, for the wonderful surprise gift!",
+//     "Thank you, Ollie, for the wonderful surprise gift!",
+//     "Thank you, Aki, for the wonderful surprise gift!",
+
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event){
+
+    let cards = [];
+    
+    for (let index = 0; index < names.length; index++) {
+        cards[index] = `Thank you, ${names[index]}, for the wonderful surprise gift!`;       
+        console.log(cards[index]);
+    }
+     
+    return cards //array of thank you messages for each name provided to the function
+}
+
+writeCards(names, "birthday")
+
+function countDown(fromNumber){
+
+    while (fromNumber >=0) {
+        console.log(fromNumber);               
+        fromNumber--        
+    }
+}
+
+countDown(10)
